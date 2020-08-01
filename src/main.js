cd9a41ad0d8912bbd8c8008a529ebf1ce4625de7
Basuke Suzuki <basuke@mac.com>
2020-07-31 21:41:17 -0700

    Merge firebase and Svelte build environment.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import App from './App.svelte';
 
-const app = new App({
-	target: document.querySelector('#app'),
-	props: {
-		name: 'Basuke'
-	}
+document.addEventListener('DOMContentLoaded', function() {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+        let app = firebase.app();
+        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+        console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+
+        const target = document.querySelector('#app');
+        // remove all contents.
+        Array.from(target.childNodes).forEach(node => node.parentNode.removeChild(node));
+        // install svelte app.
+        new App({
+            target,
+            props: {
+                name: 'Svelte and Firebase',
+                app,
+            }
+        });
+    } catch (e) {
+        console.error(`Error loading the Firebase SDK, check the console: ${e}`);
+    }
 });
 
 export default app;
