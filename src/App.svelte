9a869eab1f2515e5aa9288c9194eea4ffcd248cc
Basuke Suzuki <basuke@mac.com>
2020-07-31 21:53:17 -0700

    Added file upload form.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script>
     import WelcomeFirebase from './components/firebase-welcome.svelte';
+    import UploadForm from './components/upload-form.svelte';
 
     export let name;
     export let app;
+
+    let file;
 </script>
 
 <main>
     <h1>Hello {name}!</h1>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <UploadForm bind:selected={file} />
+    {#if file}{file.name}{/if}
     <WelcomeFirebase/>
 </main>
 
