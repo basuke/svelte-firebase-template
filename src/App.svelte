b30ae5d68b2b74b84d8cd16bb8beb16701656c52
Basuke Suzuki <basuke@mac.com>
2020-07-31 23:57:18 -0700

    Remove project info.

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,18 @@
 <script>
     import WelcomeFirebase from './components/firebase-welcome.svelte';
-    import UploadForm from './components/upload-form.svelte';
 
     export let name;
     export let app;
-
-    let file;
-    let percentage = undefined;
-    let url;
 
     const storage = firebase.storage();
     const firestore = firebase.firestore();
     const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-    $: if (file) {
-        const uploading = file;
-        file = undefined;
-
-        const ref = storage.ref(uploading.name);
-        ref.put(uploading).on('state_changed', snap => {
-            percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
-        }, err => {
-            console.error(err);
-            percentage = undefined;
-        }, async () => {
-            url = await ref.getDownloadURL();
-
-            const collection = firestore.collection('images');
-            collection.add({ url, createdAt: timestamp() })
-            percentage = undefined;
-        });
-    }
 </script>
 
 <main>
     <h1>Hello {name}!</h1>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-    <UploadForm bind:selected={file} />
-    {#if file}{file.name}{/if}
-    {#if percentage}{percentage}%{/if}
-    {#if url}<img src={url} alt="Uploaded image">{/if}
     <WelcomeFirebase/>
 </main>
 
